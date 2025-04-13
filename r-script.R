3fb7bbabf30b713e207901613cb70c84ea795414
Lutz Schubbert <102031029+luschub@users.noreply.github.com>
2025-04-13 13:15:30 +0200

    r-script.R aktualisieren

--- r-script.R
+++ r-script.R
@@ -14,8 +14,8 @@ print(R.version)
 
 # Erstelle einen einfachen Datensatz
 daten <- tibble(
-  x = 1:5000,
-  y = rnorm(5000, mean = 5, sd = 1.4)
+  x = 1:50,
+  y = rnorm(50, mean = 5, sd = 1.4)
 )
 
 # Berechne einige Statistiken
